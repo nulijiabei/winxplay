c0beabd66d9b5c234baa655943cc8d9d65047910
nljb <a@nljb.net>
2020-03-06 20:26:30 +0800

    add snap

--- example/xplayctl.go
+++ example/xplayctl.go
@@ -1,6 +1,6 @@
 ﻿package main
 
-// 20200221
+// 20200306
 
 import (
 	"encoding/json"
@@ -15,6 +15,9 @@ import (
 
 // Connect Address
 var addr = flag.String("addr", "127.0.0.1:8700", "xplay tcp address")
+
+// Snap
+var snap = flag.Bool("snap", false, "screen snapshot")
 
 // Move
 var move = flag.Bool("move", false, "move playing")
@@ -250,6 +253,12 @@ func (this *XPlay) move() error {
 	return this.send(data)
 }
 
+func (this *XPlay) snap() error {
+	data := make(map[string]interface{})
+	data["type"] = "snap"
+	return this.send(data)
+}
+
 func main() {
 
 	// 设置一下日志的结构
@@ -274,6 +283,8 @@ func main() {
 		err = xplay.play()
 	} else if *move { // Move
 		err = xplay.move()
+	} else if *snap { // Snap
+		err = xplay.snap()
 	} else if *stop { // Stop
 		if *all { // Stop ALL
 			err = xplay.stop_all()
