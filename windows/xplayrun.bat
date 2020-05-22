c98e8b825a207ee50506aa8508f6c6dd8af2a9e3
nljb <a@nljb.net>
2020-05-22 10:44:14 +0800

    sdl_image to stb_image

--- windows/xplayrun.bat
+++ windows/xplayrun.bat
@@ -3,9 +3,7 @@ goto start
 -fps 30				帧率设置
 -noloop				视频在播放到结尾时是否停留在最后一帧
 -mute				静音设置
--sample_rate 44100	音频采样频率设置
--cpuid				显示CPUID
 -version			版本号
 :start
 
-winxplay.exe -R 0,0,1920,1080 -fps 30 -sample_rate 44100 -key xplay
+winxplay.exe -R 0,0,1920,1080 -fps 30 -key xplay
